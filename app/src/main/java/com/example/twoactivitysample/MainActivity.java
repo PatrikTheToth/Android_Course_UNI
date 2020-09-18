acc8f82bd4565ce00492aa9b3bda2819c373eaab
Patrik Toth <patrikthetoth@gmail.com>
2020-09-18 10:21:53 +0200

    init

---
+++ app/src/main/java/com/example/twoactivitysample/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.twoactivitysample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupUI();
+    }
+    private void setupUI(){
+        final TextView tvTime = findViewById(R.id.tvTime);
+        Button btnPress = findViewById(R.id.btnPress);
+        btnPress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String time = new Date(System.currentTimeMillis()).toString();
+                Toast.makeText(MainActivity.this, time, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/twoactivitysample/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.example.twoactivitysample;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_second);
+    }
+}
