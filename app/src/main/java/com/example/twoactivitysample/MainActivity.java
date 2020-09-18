c31ce059ead4676de0470db97a949fe14eb92bea
Patrik Toth <patrikthetoth@gmail.com>
2020-09-18 13:41:40 +0200

    second button added

--- app/src/main/java/com/example/twoactivitysample/MainActivity.java
+++ app/src/main/java/com/example/twoactivitysample/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.twoactivitysample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String time = new Date(System.currentTimeMillis()).toString();
                 Toast.makeText(MainActivity.this, time, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnsecond = findViewById(R.id.btnNewActivity);
+        btnsecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentsecond = new Intent();
+                intentsecond.setClass( MainActivity.this, SecondActivity.class);
+                startActivity(intentsecond);
             }
         });
 
